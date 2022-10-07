a6a348ca5af030956d9e5746bd31c993482d66ca
Lê Hoàng Long <77973385+lehoanglong12345@users.noreply.github.com>
2022-10-07 08:30:23 +0700

    Initialize PJ

---
+++ src/main/java/com/example/e_learing_be/ELearningBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_learing_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e_learing_be/ELearingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_learing_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
